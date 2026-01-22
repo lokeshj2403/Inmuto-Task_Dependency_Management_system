a9f95ed8efac45fef665d6272a7214a8c96199eb
Lokesh J <lokeshjreddy2403@gmail.com>
2026-01-22 07:42:15 +0530

    Backend verified: task dependencies, cycles, status propagation

--- backend/settings.py
+++ backend/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "task_manager",
+        "USER": "lokeshj",
+        "PASSWORD": "Localhost@2403",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
+
 
 
 # Password validation
